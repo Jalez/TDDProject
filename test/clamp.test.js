7b79446c71832a624a229e29f8899bf55c22367c
Kian Moloney <104380978+k9du@users.noreply.github.com>
2024-10-11 14:24:46 +0300

    Update clamp.test.js

--- test/clamp.test.js
+++ test/clamp.test.js
@@ -2,6 +2,7 @@ import { expect } from 'chai';
 import clamp from '../COMP.SE.200-2024-2025-1/src/clamp.js';
 
 describe('clamp.js', () => {
+  // not sure whats intended behavior
   it('should clamp number within bounds', () => {
     expect(clamp(1, -5, 5)).to.equal(-5);
   });
@@ -10,10 +11,12 @@ describe('clamp.js', () => {
     expect(clamp(-10, -5, 5)).to.equal(-5);
   });
 
+  // this works straight opposite, func clamps to lower bound
   it('should clamp number above upper bound', () => {
     expect(clamp(10, -5, 5)).to.equal(-5);
   });
 
+  // made by looking at functionality, bit weird 
   it('should return lower bound if number equals lower bound', () => {
     expect(clamp(-5, -5, 5)).to.equal(-5);
   });
@@ -22,6 +25,8 @@ describe('clamp.js', () => {
     expect(clamp(5, -5, 5)).to.equal(-5);
   });
 
+  
+  // should these work all the same for NaN input?
   it('should return NaN if number is NaN', () => {
     expect(clamp(NaN, -5, 5)).to.be.NaN;
   });
